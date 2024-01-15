def7a792298dab79bfa94d40e0bdede82afe80cd
Olexander <alex.pilyavskiy03@gmail.com>
2024-01-15 18:27:41 +0200

    done task 1

---
+++ objects/task1.js
@@ -0,0 +1,23 @@
+/*Создай объект, представляющий пользователя, 
+с полями для имени, возраста и почтового адреса. 
+Затем добавьте, измените и удалите несколько свойств.
+*/
+
+const userProfile = {
+  firstName: "Oleksandr",
+  secondName: "Piliavskyi",
+  age: 22,
+  email: "alex.pilyavskiy03@gmail.com",
+};
+
+console.log(userProfile);
+
+userProfile.work = "System administrator";
+userProfile.experince = 2;
+userProfile.hasGirlFriend = false;
+
+console.log(userProfile);
+
+delete userProfile.work;
+
+console.log(userProfile);
